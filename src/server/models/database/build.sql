475f9d45b909819e56a131cd6d9ebdb0a329dcd8
yahya <yalbarrawoui256@gmail.com>
2018-03-12 14:50:35 +0200

    changes solved

--- src/server/models/database/build.sql
+++ src/server/models/database/build.sql
@@ -4,28 +4,26 @@ BEGIN;
 DROP TABLE IF EXISTS users_table, users_info CASCADE;
 
 CREATE TABLE "users_table" (
-	"id" SERIAL NOT NULL,
+	"id" SERIAL PRIMARY KEY,
 	"username" TEXT NOT NULL,
 	"role" TEXT NOT NULL,
-	"reg_date" DATE NOT NULL,
-	CONSTRAINT users_table_pk PRIMARY KEY ("id")
+	"reg_date" DATE NOT NULL
 );
 
 
 CREATE TABLE "users_info" (
-	"id" SERIAL NOT NULL,
+	"id" SERIAL PRIMARY KEY,
 	"user_id" INTEGER REFERENCES users_table(id),
 	"campus" TEXT NOT NULL,
 	"cohort" TEXT NOT NULL,
-	"interests" varchar NOT NULL,
+	"interests" VARCHAR NOT NULL,
 	"skills" TEXT NOT NULL,
 	"cv" TEXT NOT NULL,
-	"login_date" TEXT NOT NULL,
+	"login_date" DATE NOT NULL,
 	"status" TEXT NOT NULL,
 	"social_links" TEXT NOT NULL,
 	"portfolio" TEXT NOT NULL,
-	"projects" TEXT NOT NULL,
-	CONSTRAINT users_info_pk PRIMARY KEY ("id")
+	"projects" TEXT NOT NULL
 );
 
 
