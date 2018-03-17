33a51fed61fbb29a82e6719a34e46cd9378b4c71
yasminhillis <jasminehillis94@gmail.com>
2018-03-17 13:13:02 +0200

    edit homepage

--- src/client/components/home/index.js
+++ src/client/components/home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../../../../routes/authRoutes';
 
 import './index.css';
 
@@ -20,7 +21,8 @@ class Home extends Component {
             <div className='home__text'>Founders & Coders
               sustainable opportunity-based Community</div>
           </div>
-          <input className='home__btn' type='submit' value='Sign in with' />
+          <a className='home__btn' href="/auth/github">Sign in with</a>
+          // <input className='home__btn' href="/auth/github" type='submit' value='Sign in with' />
         </div>
       </div>
     );
