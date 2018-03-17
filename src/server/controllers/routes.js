6dfe47ceaee554dc64f1a001bf582420b324eb93
yasminhillis <jasminehillis94@gmail.com>
2018-03-17 13:17:38 +0200

    create auth routes

--- src/server/controllers/routes.js
+++ src/server/controllers/routes.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
+router.get('/auth/github',
+      passport.authenticate('github',
+      {scope: ['email']})
+);
+
+router.get('/auth/gitgub/callback',
+      passport.authenticate('github'),
+      (req,res)=>{
+        res.redirect('/')
+      }
+    );
+
+router.get('/api/current_user',(req,res)=>{
+        res.send(req.user);
+      });
+
+
+router.get('/api/logout',(req,res)=>{
+        req.logout();
+        res.redirect('/');
+        });
+
 
 module.exports = router;
