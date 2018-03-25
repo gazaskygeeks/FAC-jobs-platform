904c7b210679eb45c9799ffe13743ebb3d67455f
yahya <yalbarrawoui256@gmail.com>
2018-03-25 13:31:50 +0200

    log deletions

--- src/server/controllers/studentProfile.js
+++ src/server/controllers/studentProfile.js
@@ -1,9 +1,7 @@
 const view = require('../models/queries/studentDataQuery.js');
 exports.get = (req, res) => {
   const student_id = req.params.student_id;
-  console.log('student id', student_id);
   view.studentData(student_id,(dataBaseConnectionErorr, studentData) => {
-    console.log('studentData', studentData);
     if (dataBaseConnectionErorr) res.status(500).send(dataBaseConnectionErorr);
     else {
       if (studentData.length === 0) {
