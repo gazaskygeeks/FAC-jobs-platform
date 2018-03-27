a4959e09d3e727298be86686d0d86881996437ff
Yahya Albarrawi <yalbarrawoui256@gmail.com>
2018-03-27 08:17:38 +0200

    Revert "Local database"

--- src/server/models/database/dbConnection.js
+++ src/server/models/database/dbConnection.js
@@ -1,14 +1,8 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
-let data_url;
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DB url not found ');
-}
-if (process.env.NODE_ENV === 'test') {
-  data_url = process.env.DATABASE_URL_TEST;
 } else {
-  data_url = process.env.DATABASE_URL;
+  module.exports = new Pool({ connectionString: process.env.DATABASE_URL, ssl: true });
 }
-
-module.exports = new Pool({ connectionString: data_url, ssl: true });
