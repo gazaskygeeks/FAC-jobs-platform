f99018f05d2af4ba80b47e5bf75d84bd3a00ba7c
walaa <walaa@localhost.localdomain>
2018-04-17 14:59:59 -0400

    update settings query

--- src/server/models/queries/updateDataQueries.js
+++ src/server/models/queries/updateDataQueries.js
@@ -4,15 +4,11 @@ module.exports = (dataUserToUpdate,cb) => {
   const sql = `BEGIN;
    UPDATE users_table
    SET email = '${dataUserToUpdate.email}',display_name = '${dataUserToUpdate.display_name}'
-   FROM users_table T1, users_info T2
-   WHERE T1.id = T2.user_id
-   and T1.id = ${dataUserToUpdate.id};
+   WHERE id = ${dataUserToUpdate.id};
 
    UPDATE users_info
    SET interests = '{${dataUserToUpdate.interests}}',skills='{${dataUserToUpdate.skills}}'
-   FROM users_table T1, users_info T2
-   WHERE T1.id = T2.user_id
-   and T1.id = ${dataUserToUpdate.id};
+   WHERE  user_id = ${dataUserToUpdate.id};
 
    COMMIT;`;
   connect.query(sql, cb);
