395bba8260aa93af3a5182b96446869ea9237579
yahya <yalbarrawoui256@gmail.com>
2018-05-02 15:07:12 +0300

    basic files

--- src/client/actions/sortStudentsAction.js
+++
@@ -1,9 +0,0 @@
-import { SORT_STUDENTS_SUCCESS } from '../constants/actionTypes.js';
-
-export default (data,sortdata) => {
-  return {
-    type: SORT_STUDENTS_SUCCESS,
-    payload: { data,sortdata }
-  };
-
-}; ;
